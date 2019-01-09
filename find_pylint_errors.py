497b65f1a7d2b82ad714ed54fa941b64669ed1f9
swrni <swrni.1@gmail.com>
2019-01-09 13:06:40 +0200

    find pylint update

--- find_pylint_errors.py
+++ find_pylint_errors.py
@@ -1,95 +1,83 @@
-"""Kvak."""
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# vim: ts=4 sw=4 tw=100 et ai si
 
-#pylint: disable=invalid-name
+"""Tool that runs pylint on python files in a chosen directory."""
 
 import os
 import sys
-# # from powerlablibs import Exceptions
-# from helperlibs import Procs, Logging
-from helperlibs import ArgParse, Procs
+import argparse
 
-VERSION = "0.1"
+from process import Process
+
 OWN_NAME = "find_pylint_errors"
-
-def iterate_files(directory, filter_list):
-    """
-    Walk through all the directories in 'directory' and yield each file. Path names in
-    'filter_list' are skipped.
-    """
-
-    # path_prefix = os.path.relpath(directory, os.getcwd())
-    for dir_path, dir_names, filenames in os.walk(directory):
-        # Filter directories from dir_path.
-        for index, path in enumerate(dir_names):
-            if path in filter_list:
-                del dir_names[index]
-        # Iterate files in dir_path.
-        for path in filenames:
-            yield os.path.join(dir_path, path)
-
-def is_python_file(path):
-    """Return boolean value indicating if 'path' points to a python file."""
-
-    if path.endswith(".py"):
-        return True
-    try:
-        with open(path, errors="ignore") as open_file:
-            if "python" in open_file.readline():
-                return True
-    except FileNotFoundError as err:
-        raise RuntimeError("'%s' cannot be opened:\n\t %s" % (path, err), file=sys.stderr)
-
-    return False
 
 def parse_arguments():
     """Parse arguments to get the subdirectory."""
 
-    directory = os.getcwd()
+    def parse_list(items):
+        """Make 'items' string into list of comma separated items."""
 
-    arguments = sys.argv[1:]
-    if arguments:
-        directory = os.path.join(directory, arguments[0])
-
-    return directory
-
-    """A helper function which parses the input arguments."""
+        return {item.strip() for item in items.split(",")}
 
     text = sys.modules[__name__].__doc__
-    parser = ArgParse.ArgsParser(description=text, prog=OWN_NAME, ver=VERSION)
-    subparsers = parser.add_subparsers(title="commands", metavar="")
+    parser = argparse.ArgumentParser(description=text, prog=OWN_NAME)
+    parser.add_argument("--only", help="Show only these pylint warnings.", type=parse_list)
+    parser.add_argument("directory", help="Find files inside this directory.", default="",
+                        nargs="?")
+    return parser.parse_args(sys.argv[1:])
 
-    text = "Clone all Server Power Lab projects."
-    subparsers.add_parser("clone", help=text)
+def get_files(args):
+    """Call 'find_python_files.py' and return python files."""
 
-    text = "A git command to run in every Server Power Lab project"
-    subparsers.add_parser("<cmd>", help=text)
-    return parser.parse_args()
+    filter_list = [".git", "__pycache__"]
+    find_files_command = "python ./find_python_files.py"
+    if args.debug:
+        find_files_command += " -d"
+
+    if filter_list:
+        find_files_command += " -f \"%s\"" % ", ".join(filter_list)
+
+    find_files_command += " %s" % args.directory
+    if args.debug:
+        print(find_files_command)
+
+    stdout, stderr, _ = Process().run(find_files_command) # , cwd=os.getcwd())
+    if stderr:
+        print(stderr, file=sys.stderr)
+    return stdout
 
 def main():
     """Entry point."""
 
-    directory = parse_arguments()
+    args = parse_arguments()
+
+    directory = args.directory
     if not os.path.isdir(directory):
         print("Pass subdirectory to find python files.", file=sys.stderr)
         return 0
 
-    print("directory: %s" % directory)
+    if args.debug:
+        print("directory: %s" % directory)
 
-    filter_list = [".git", "__pycache__"]
+    python_files = get_files(args)
+    if args.debug:
+        print(python_files)
 
-    python_files = [file for file in iterate_files(directory, filter_list)
-                    if is_python_file(file)]
+    command = "pylint --output-format=colorized --score=no --jobs=0 --verbose %s" % python_files
 
-    for path in python_files:
-        print(path)
-    print()
+    stdout, stderr, _ = Process().run(command)
+    if args.only:
+        for line in stdout.split("\n"):
+            if any([warning in line for warning in args.only]):
+                print(line)
+    else:
+        print(stdout)
 
-    command = "pylint --output-format=colorized --score=no --jobs=0 --verbose %s" \
-              % " ".join(python_files)
+    if stderr:
+        print(stderr, file=sys.stderr)
 
-    stdout, stderr, _ = Procs.run(command)
-    print(stdout)
-    print(stderr, file=sys.stderr)
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main())
